dab0f072845289f71eff7c277defc87262aa3b66
Subodh Kumar Singh <106647817+subodhrathour17@users.noreply.github.com>
2025-03-18 23:23:51 +0530

    Update Error.js

--- src/pages/Error.js
+++ src/pages/Error.js
@@ -1,11 +1,35 @@
 import React from "react";
+import styled from "styled-components";
 
-function Error() {
+const ErrorContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
+  background-color: #f8d7da;
+  color: #721c24;
+  font-family: Arial, sans-serif;
+`;
+
+const ErrorTitle = styled.h1`
+  font-size: 3rem;
+  margin-bottom: 20px;
+`;
+
+const ErrorMessage = styled.p`
+  font-size: 1.2rem;
+`;
+
+const Error = () => {
   return (
-    <div>
-      <h1>Page Not Found 400</h1>
-    </div>
+    <ErrorContainer>
+      <ErrorTitle>Page Not Found - 404</ErrorTitle>
+      <ErrorMessage>
+        The page you are looking for might have been removed or doesn't exist.
+      </ErrorMessage>
+    </ErrorContainer>
   );
-}
+};
 
 export default Error;
